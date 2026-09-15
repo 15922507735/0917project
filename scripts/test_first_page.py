59fb805e2ce8c692f11cbb0a835efd5f25cdc771
王钟磊 <9266493+wlxl1106@user.noreply.gitee.com>
2026-09-15 12:38:24 +0800

    qiyuantest

---
+++ scripts/test_first_page.py
@@ -0,0 +1,9 @@
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import pytest
+
